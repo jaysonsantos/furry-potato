43da9ef46180c48740a5fda4d251bbc5aee554ed
Jayson Reis <santosdosreis@gmail.com>
2022-06-01 21:38:31 +0200

    Add test for multiple dispute resolve
    Signed-off-by: Jayson Reis <santosdosreis@gmail.com>

--- cli/tests/flow.rs
+++ cli/tests/flow.rs
@@ -5,7 +5,7 @@ use tokio::{fs::File, io::BufWriter, test};
 use tokio_stream::StreamExt;
 use transaction::client::ClientPosition;
 
-macro_rules! test_cases {
+macro_rules! positive_test_cases {
     ($input_file:ident) => {
         #[test]
         async fn $input_file() {
@@ -16,18 +16,38 @@ macro_rules! test_cases {
         }
     };
     ($input_file:ident, $($input_files:ident),+) => {
-        test_cases!($input_file);
-        test_cases!($($input_files),+);
+        positive_test_cases!($input_file);
+        positive_test_cases!($($input_files),+);
     }
 }
 
-test_cases!(
+positive_test_cases!(
     big_decimals,
     chargeback,
     open_dispute,
     resolve_dispute,
     shifted_columns
 );
+
+macro_rules! negative_test_cases {
+    ($input_file:ident) => {
+        #[test]
+        async fn $input_file() {
+            let input_filename = format!("../fixtures/{}.csv", stringify!($input_file));
+            let output_filename = format!("../fixtures/{}-output.csv", stringify!($input_file));
+            setup_instrumentation();
+            let err = test_case_impl(&input_filename, &output_filename).await.unwrap_err();
+            let expected_error = tokio::fs::read_to_string(output_filename).await.unwrap();
+            assert_eq!(format!("{}", err.root_cause()), expected_error.trim());
+        }
+    };
+    ($input_file:ident, $($input_files:ident),+) => {
+        negative_test_cases!($input_file);
+        negative_test_cases!($($input_files),+);
+    }
+}
+
+negative_test_cases!(multiple_resolve_dispute);
 
 async fn test_case_impl(input_filename: &str, output_filename: &str) -> Result<()> {
     let client = Cli::new().expect("should create client");
@@ -53,7 +73,7 @@ async fn test_case_impl(input_filename: &str, output_filename: &str) -> Result<(
         client
             .process_and_print_transactions(input_file, &mut output_writer)
             .await
-            .expect("failed to process fixture");
+            .wrap_err("failed to process fixture")?;
     }
 
     let output = output_writer.into_inner();
@@ -65,7 +85,7 @@ async fn test_case_impl(input_filename: &str, output_filename: &str) -> Result<(
         .deserialize::<ClientPosition>()
         .collect::<Result<Vec<_>, _>>()
         .await
-        .expect("failed to read output into structs");
+        .wrap_err("failed to read output into structs")?;
     for (i, (expected, output)) in expected_output.iter().zip(output.iter()).enumerate() {
         assert_eq!(
             expected,
